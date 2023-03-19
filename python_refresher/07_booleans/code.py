5c3a8883458659c4231fc52a8941e87a39917f8d
lyamlim97 <lyamlim97@gmail.com>
2023-03-19 18:48:07 +0800

    added 07_booleans code

---
+++ python_refresher/07_booleans/code.py
@@ -0,0 +1,12 @@
+# comparisons
+print(5 == 5)
+print(5 > 5)
+print(10 != 10)
+
+
+# 'is' keyword
+friends = ['Rolf', 'Bob']
+abroad = ['Rolf', 'Bob']
+
+print(friends == abroad)
+print(friends is abroad)
