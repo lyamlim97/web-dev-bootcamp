2b6398127a13249053ff6909825ccab4cf1f449c
lyamlim97 <lyamlim97@gmail.com>
2023-03-22 18:57:16 +0800

    added 31_relative_imports_python code

---
+++ python_refresher/31_relative_imports_python/libs/mylib.py
@@ -0,0 +1,6 @@
+from libs.operations import operator
+
+print('mylib.py:', __name__)
+
+
+from .operations import operator
---
+++ python_refresher/31_relative_imports_python/mymodule.py
@@ -0,0 +1,13 @@
+from libs import mylib
+
+print('mymodule.py:', __name__)
+
+
+def divide():
+	pass
+
+
+from .libs import mylib
+
+	
+import libs.operations.operator
