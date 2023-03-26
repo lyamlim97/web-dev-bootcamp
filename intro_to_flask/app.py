f92e8de14726d6509965908e866f7c7cbfeb647c
lyamlim97 <lyamlim97@gmail.com>
2023-03-26 17:27:40 +0800

    removed second page, changed first page to jinja intro page

--- intro_to_flask/app.py
+++ intro_to_flask/app.py
@@ -5,9 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('first_page.html')
-
-
-@app.route('/second')
-def hello_world_fancy():
-    return render_template('second_page.html')
+    return render_template('jinja_intro.html', name='Bob Smith', template_name='Jinja2')
