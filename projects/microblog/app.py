3217b13497efe3ea3a0722c0a57901eb01166ea8
lyamlim97 <lyamlim97@gmail.com>
2023-03-27 02:41:52 +0800

    added code to store entries on mongodb

--- projects/microblog/app.py
+++ projects/microblog/app.py
@@ -1,7 +1,14 @@
 import datetime
+import json
 from flask import Flask, render_template, request
+from pymongo import MongoClient
+
+with open('secrets.json', 'r') as read_file:
+    secrets = json.load(read_file)
 
 app = Flask(__name__)
+client = MongoClient(secrets["MONGODB_URL"])
+app.db = client.microblog
 
 entries = []
 
@@ -12,8 +19,10 @@ def home():
         entry_content = request.form.get('content')
         formatted_date = datetime.datetime.today().strftime('%Y-%m-%d')
         entries.append((entry_content, formatted_date))
+        app.db.entries.insert(
+            {'content': entry_content, 'date': formatted_date})
 
     entries_with_date = [
-        (entry[0], entry[1], datetime.datetime.strptime(entry[1], '%Y-%m-%d').strftime('%b %d')) for entry in entries
+        (entry['content'], entry['date'], datetime.datetime.strptime(entry[1], '%Y-%m-%d').strftime('%b %d')) for entry in entries
     ]
     return render_template('home.html', entries=entries_with_date)
