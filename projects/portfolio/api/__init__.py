3c890590232c1633b7c3b4dc001a32fbc6693532
lyamlim97 <lyamlim97@gmail.com>
2023-04-01 19:24:26 +0800

    add content and styling for projects page

--- projects/portfolio/api/__init__.py
+++ projects/portfolio/api/__init__.py
@@ -2,10 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+projects = [
+    {
+        'name': 'Habit tracking app with Python and MongoDB',
+        'thumb': 'img/habit-tracking.png',
+        'hero': 'img/habit-tracking-hero.png',
+        'categories': ['python', 'mongodb'],
+        'slug': 'habit-tracking',
+        'prod': 'www.google.com',
+    },
+    {
+        'name': 'Microblog app with Python and MongoDB',
+        'thumb': 'img/personal-finance.png',
+        'hero': 'img/personal-finance.png',
+        'categories': ['python', 'mongodb', 'writing'],
+        'slug': 'microblog',
+        'prod': 'www.google.com',
+    }
+]
+
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', projects=projects)
 
 
 @app.route('/about')
