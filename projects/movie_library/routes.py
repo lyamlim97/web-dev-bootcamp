e203aa4a69085962673980c72da2bbe5d07b8019
lyamlim97 <lyamlim97@gmail.com>
2023-04-02 17:18:05 +0800

    add nav bar html code and css styling

--- projects/movie_library/routes.py
+++ projects/movie_library/routes.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template
 
-pages = Blueprint("pages", __name__,
-                  template_folder="templates", static_folder="static")
+pages = Blueprint('pages', __name__,
+                  template_folder='templates', static_folder='static')
 
 
-@pages.route("/")
+@pages.route('/')
 def index():
-    return render_template("index.html", title="Movies Watchlist",)
+    return render_template('index.html', title='Movies Watchlist')
