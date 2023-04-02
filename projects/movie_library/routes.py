c0f6c2b3d09ecc8b265bbe5117f7013d7a6d4057
lyamlim97 <lyamlim97@gmail.com>
2023-04-02 18:32:20 +0800

    add dark and light mode toggle

--- projects/movie_library/routes.py
+++ projects/movie_library/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, session
 
 pages = Blueprint('pages', __name__,
                   template_folder='templates', static_folder='static')
@@ -7,3 +7,14 @@ pages = Blueprint('pages', __name__,
 @pages.route('/')
 def index():
     return render_template('index.html', title='Movies Watchlist')
+
+
+@pages.get('/toggle-theme')
+def toggle_theme():
+    current_theme = session.get('theme')
+    if current_theme == 'dark':
+        session['theme'] = 'light'
+    else:
+        session['theme'] = 'dark'
+
+    return redirect(request.args.get('current_page'))
